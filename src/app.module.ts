393b010a89411327c459385176c91888cef40be2
asfya <abeyrami2005@gmail.com>
2024-12-31 01:25:33 -0800

    add project

---
+++ src/app.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { UsersModule } from './application/users/users.module';
+
+@Module({
+  imports: [UsersModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
